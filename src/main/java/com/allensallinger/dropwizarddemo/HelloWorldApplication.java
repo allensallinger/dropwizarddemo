7aeb55416963bd6eb100cdc43b0a70e9b7586e95
allensallinger <asalling@uci.edu>
2018-01-23 10:59:58 -0800

    initial commit

---
+++ src/main/java/com/allensallinger/dropwizarddemo/HelloWorldApplication.java
@@ -0,0 +1,35 @@
+package com.allensallinger.dropwizarddemo;
+
+import com.allensallinger.dropwizarddemo.resources.HelloWorldResource;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloWorldApplication  extends Application<HelloWorldConfiguration>{
+    private static String[] args;
+
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+
+    }
+
+    @Override
+    public void run(com.allensallinger.dropwizarddemo.HelloWorldConfiguration configuration,
+                    Environment environment) {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+        environment.jersey().register(resource);
+    }
+
+}
---
+++ src/main/java/com/allensallinger/dropwizarddemo/api/Saying.java
@@ -0,0 +1,31 @@
+package com.allensallinger.dropwizarddemo.api;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Length;
+
+public class Saying {
+    private long id;
+
+    @Length
+    private String content;
+
+    public Saying() {
+
+    }
+
+    public Saying(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    @JsonProperty
+    public long getId() {
+        return id;
+    }
+
+    @JsonProperty
+    public String getContent() {
+        return content;
+    }
+
+}
